20951cf4779006b0779e78f96d826c01402bc749
TrHoangDuy <123785872+Duykyo@users.noreply.github.com>
2025-10-23 09:15:05 +0700

    scale logic

--- Role.java
+++ Role.java
@@ -1,18 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package racb;
-
-/**
- *
- * @author Administrator
- */
+import java.util.Set;
+import java.util.HashSet;
 public class Role {
     private String roleName;
+    private Set<Role> inheritedRoles;
     
     public Role(String name){
         this.roleName = name;
+        this.inheritedRoles = new HashSet<>();
+    }
+    public void addInheritedRoles(Role parentRole){
+        this.inheritedRoles.add(parentRole);
+    }
+    public Set<Role> getInheritedRoles(){
+        return this.inheritedRoles;
     }
     public String getName() {
         return roleName;
